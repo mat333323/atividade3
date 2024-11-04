76a18b6fef7d7629531b69546ca3d0112b9ffea6
mat333323 <12302988@aluno.cotemig.com.br>
2024-11-04 12:47:20 -0300

    Add files via upload

---
+++ modal - Copia.js
@@ -0,0 +1,38 @@
+const abrirmodal = document.querySelector("#open-modal");
+
+const fecharmodal = document.querySelector("#fechar");
+
+const modal = document.querySelector("#modal");
+
+const fade = document.querySelector("#modal-fade");
+
+
+
+
+function abrirFechar(){
+
+
+
+    modal.showModal()
+
+
+
+}
+
+fecharmodal.addEventListener("click", () => {
+  modal.close();}
+  )
+
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var element = dialog.body;
+    element.classList.toggle("light-mode");
+
+
+
+
+
+ }
+
+
---
+++ port.js
@@ -0,0 +1,23 @@
+var modal = document.querySelector("#modal");
+var modalOverlay = document.querySelector("#modal-overlay");
+var closeButton = document.querySelector(".but3");
+
+
+function toggleMenu() {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+};
+
+but3.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
+
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("light-mode");
+}
+
+
+
